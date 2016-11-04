ca4e1c1c06ff8a3e9c53cc03449d026455845a4c
Defiancefew <Severancepwns@gmail.com>
2016-11-04 15:28:14 +0300

    Reconfigured webpack for better dev experience.

--- server.js
+++ server.js
@@ -2,40 +2,44 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import { spawn } from 'child_process';
+import path from 'path';
 
 import config from './webpack.config.development';
 
+const argv = require('minimist')(process.argv.slice(2));
+
 const app = express();
 const compiler = webpack(config);
-const PORT = 3000;
+const PORT  = process.env.PORT || 3000;
 
-app.use(webpackDevMiddleware(compiler, {
+const wdm = webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
-  watchOptions: {
-    poll: true
-  },
   stats: {
     colors: true
   }
-}));
+});
 
+app.use(wdm);
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+const server = app.listen(PORT, 'localhost', (serverError) => {
+  if (serverError) {
+    return console.error(serverError);
+  }
 
-const server = app.listen(PORT, 'localhost', (err) => {
-  if (err) {
-    console.error(err);
-    return;
+  if (argv['start-hot']) {
+    spawn('npm', ['run', 'start-hot'], { shell: true, env: process.env, stdio: 'inherit' })
+      .on('close', code => process.exit(code))
+      .on('error', spawnError => console.error(spawnError));
   }
 
   console.log(`Listening at http://localhost:${PORT}`);
 });
 
-
 process.on('SIGTERM', () => {
   console.log('Stopping dev server');
-  webpackDevMiddleware.close();
+  wdm.close();
   server.close(() => {
     process.exit(0);
   });
